847e029bf9e87d4d11b0c1c490dee6eeaafa6734
l3u9 <l3u9@github.com>
2022-09-18 08:43:06 -0700

    save chapter 2

---
+++ EthicalRAT/fun.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+
+
+BOOL APIENTRY DllMain(HMODULE Base, DWORD Callback, LPVOID Param)
+{
+	switch (Callback)
+	{
+	case DLL_PROCESS_ATTACH:
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	default:
+		break;
+	}
+	return 1;
+}
+
+extern "C" __declspec(dllexport) int FunEntry()
+{
+	return MessageBoxA(0, "Hello from fun dll", 0, 0);
+}
